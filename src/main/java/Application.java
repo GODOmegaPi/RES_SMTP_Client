c32ca943aab39aa0132e93738330dac0f82d0c6a
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2019-04-01 14:31:13 +0200

    Woring client sending emails

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,14 +1,26 @@
 import java.io.IOException;
-import java.util.logging.Logger;
 
 public class Application {
-    private final static Logger LOG = Logger.getLogger(Application.class.getName());
-
     public static void main(String[] args){
         try {
             Client client = new Client();
             client.startConnection("smtp.mailtrap.io", 2525);
             client.sendMessage("EHLO smtp.mailtrap.io");
+
+            client.sendMessage("AUTH LOGIN");
+            client.sendMessage("MTUzOGIyMTliNzUxNjM=");
+            client.sendMessage("ODY2ODk0ODY3ZjIwMDY=");
+
+            client.sendMessage("MAIL FROM: <from@smtp.mailtrap.io>");
+            client.sendMessage("RCPT TO: <to@smtp.mailtrap.io>");
+            client.sendMessage("DATA");
+            client.sendMessage("To: to@smtp.mailtrap.io\n" +
+                    "From: from@smtp.mailtrap.io\n" +
+                    "Subject: Hello world!\n" +
+                    "\n" +
+                    "This is the test message...\n" +
+                    ".");
+            client.sendMessage("quit");
         } catch (IOException e){
             e.printStackTrace();
         }
--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -3,12 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.logging.Logger;
 
 public class Client {
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
+    private final String CRLF = "\r\n";
 
     public void startConnection(String ip, int port) throws IOException {
         clientSocket = new Socket(ip, port);
@@ -17,11 +17,30 @@ public class Client {
     }
 
     public void sendMessage(String msg) throws IOException {
-        out.println(msg);
+        System.out.println(msg);
+        out.print(msg + CRLF);
+        out.flush();
         String resp;
-        while((resp = in.readLine()) != null){
+        while((resp = in.readLine()) != null && !matchEndOfInstrcutions(resp)){
             System.out.println(resp);
         }
+        System.out.println(resp);
+    }
+
+    public boolean matchEndOfInstrcutions(String instruction){
+        if (instruction.startsWith("250 ")){
+            return true;
+        }else if (instruction.startsWith("334 ")){
+            return true;
+        }else if (instruction.startsWith("235 ")){
+            return true;
+        }else if (instruction.startsWith("354 ")){
+            return true;
+        }else if (instruction.startsWith("221 ")){
+            return true;
+        }
+
+        return false;
     }
 
     public void stopConnection() throws IOException {
