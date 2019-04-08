2193f9e3493b4c9b229d60889f9761701ac62a8b
GODOmegaPi <guillaume.laubscher@heig-vd.ch>
2019-04-08 13:53:09 +0200

    Added variables to easly change parameters of our program

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,18 @@
 import java.util.ArrayList;
 
 public class Application {
+    private static final String EMAILS_PATH = "emailsExample.txt";
+    private static final String MESSAGES_PATH = "messagesExample.txt";
+    private static final String SERVER_IP = "127.0.0.1";
+    private static final int SERVER_PORT = 2525;
+
+    private static final boolean USE_AUTH = false;
+    private static final String AUTH_LOGIN = "";
+    private static final String AUTH_PASSWORD = "";
+
     public static void main(String[] args){
-        String emailsPath = "emailsExample.txt";
-        String messagesPath = "messagesExample.txt";
+        String emailsPath = EMAILS_PATH;
+        String messagesPath = MESSAGES_PATH;
 
         EmailList emailList = new EmailList(emailsPath);
 
@@ -23,8 +32,10 @@ public class Application {
         System.out.println(groups);
 
         for(Group group : groups) {
-            Spam spam = new Spam("127.0.0.1", 2525, group);
-            //spam.setAuthInfos("MTUzOGIyMTliNzUxNjM=", "ODY2ODk0ODY3ZjIwMDY=");
+            Spam spam = new Spam(SERVER_IP, SERVER_PORT, group);
+            if(USE_AUTH) {
+                spam.setAuthInfos(AUTH_LOGIN, AUTH_PASSWORD);
+            }
             spam.start();
         }
     }
